e25c343de9758fb1263dc85453c874eb5590cbdb
Leonardo <160722347+cicixgliamici@users.noreply.github.com>
2024-11-02 14:54:18 +0100

    Update ex1.c

--- OperativeSystems2/lab/lab1/ex1.c
+++ OperativeSystems2/lab/lab1/ex1.c
@@ -1,4 +1,4 @@
-//  esercitazione som 1.1
+//  !! GIVEN EXAMPLE FROM UNIVERSITY !!
 
 #include <pthread.h>
 #include <stdio.h>
@@ -8,55 +8,53 @@
 #define N 20
 #define K 4
 
+//Given an empty array of N=20 elements, we will divide it in K=4 portions
+//Then we will randomize it and we will create M=N/K=5 threads
+//We will search each maximum in the M set and then find the maximum above all sets
 int V[N];
 
 
-void *Calcolo(void *t) // codice worker
-{   int first, result=0;
+void *Calcolo(void *t) {                               //"Worker", the segment where we search the maximum 
+    int first, result=0;
     first = (int)t;
     for (int i=first; i<first+K; i++)
 	if (V[i]>result)
 		result=V[i];
-    //printf("Worker ha calcolato il massimo locale: %d\n", result); 
+    printf("Local Maximum: %d\n", result); 
     pthread_exit((void*) result);
 }
 
 
-int main (int argc, char *argv[])
-{   pthread_t thread[N/K];
+int main (int argc, char *argv[]) {
+    pthread_t thread[N/K];
     int rc;
     int M, t, first, status, max=0;
-	
-    M=N/K;	
-    srand(time(0)); 
-    printf("inizializzazione vettore V:\n");
-	for(int i = 0; i < N; i++){
-	   V[i]=1+rand()%200; //numeri casuali tra 1 e 200
+    M=N/K;	                                      //Number of threads/worker
+    srand(time(0));                                   //Randomize using the seed "time"
+    printf("Vector initialization V:\n");
+    for(int i = 0; i < N; i++){
+	   V[i]=1+rand()%200;                         //Random numbers between 1-200
 	   printf("%d\t", V[i]);
    	}	
     printf("\n");
-    
     for(t=0; t<M; t++) {
-        printf("Main: creazione thread n.%d\n", t);
-	first=t*K; // passo ad ogni thread l'indice del primo elemento da elaborare
+        printf("Main: Thread creation n.%d\n", t);
+	first=t*K;                                                       //Giving each thread the index of the first element of the sets they will use
         rc = pthread_create(&thread[t], NULL, Calcolo, (void *)first);
         if (rc) {
-            printf("ERRORE: %d\n", rc);
-            exit(-1);   }
+            printf("Error: %d\n", rc);
+            exit(-1); 
+	}
     }
-	
-
     for(t=0; t<M; t++) {
         rc = pthread_join(thread[t], (void *)&status);
 	if (rc)
-		printf("ERRORE join thread %ld codice %d\n", t, rc);
+		printf("ERROR join thread %ld code %d\n", t, rc);
 	else {
-		printf("Finito thread %ld con ris. %d\n",t,status);
+		printf("Finished thread %ld with answer %d\n",t,status);
 		if (status>max)
 			max=status;
 	}
    }
-   printf("main-risultato finale: %d\n", max);
-
-
-    }
+   printf("True answer: %d\n", max);
+}
