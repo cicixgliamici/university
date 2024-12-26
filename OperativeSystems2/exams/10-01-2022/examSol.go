63bd7e61a80acf36ce55591a5a313a9c52e482f2
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2024-12-26 20:53:00 +0100

    Update examSol.go

--- OperativeSystems2/exams/10-01-2022/examSol.go
+++ OperativeSystems2/exams/10-01-2022/examSol.go
@@ -26,7 +26,9 @@ const (
 )
 
 //Channels for synchronization and communication
-//Syntax:     reader <- sender
+//General syntax:                             reader <- sender
+//Waiting ack (no interest for the data):     <- sender
+//Saving the information:                     variable (may be another channel) <- sender
 var (
 	doneTask            =make(chan bool)           //Signals task completion
 	doneWarehouse       =make(chan bool)           //Signals warehouse shutdown
