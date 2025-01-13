b8613bf3e4678f6eb15bb96143499335f16a145b
Leonardo Chiaretti <160722347+cicixgliamici@users.noreply.github.com>
2025-01-13 19:31:01 +0100

    Update sol2.c

--- OperatingSystems2/lab/lab1/sol2.c
+++ OperatingSystems2/lab/lab1/sol2.c
@@ -1,6 +1,3 @@
-//  !! GIVEN EXAMPLE FROM UNIVERSITY - ONLY SLIGHT CHANGES !!
-
-//Rispetto al primo esercizio dobbiamo introdurre un ulteriore meccanismo di sincronizzazione oltre il semaforo, la barriera
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
@@ -10,123 +7,132 @@
 #define NUM_THREADS 9
 #define K 2
 
-typedef struct{
-	int voti[K];
-	char film[K][40];
-	int pareri;
-	int indiceVincitore;
-	pthread_mutex_t m;
-} sondaggio;
-
+// Data structure for storing survey information
 typedef struct {
-	int completati;
-	sem_t mb;           
-	sem_t barriera;     
-}barriera_sincro;
+    int ratings[K];               
+    char movies[K][40];           
+    int opinions_count;           
+    int winning_index;           
+    pthread_mutex_t mutex;        
+} survey;
 
-sondaggio S;
-barriera_sincro B;
+// Data structure for implementing a barrier synchronization mechanism
+typedef struct {
+    int completed;                
+    sem_t barrier_mutex;          
+    sem_t barrier_gate;           
+} barrier_sync;
 
-//Vedi sol1.c
-void init(sondaggio *s) {
-	int i;
-	s->pareri=0;
-	for(i=0;i<K; i++) {
-		printf("Qual è il nome del film numero %d ? ", i+1);
-		scanf("%s",s->film[i]);
-		s->voti[i]=0;
-	}
-	pthread_mutex_init(&s->m, NULL); 
-}
+survey Survey;
+barrier_sync Barrier;
 
-//Inizializzare la barriera come vuota
-void initBarriera(barriera_sincro *b) {
-  sem_init(&b->mb,0, 1);
-  sem_init(&b->barriera,0, 0);
-  b->completati=0;
-}
-
-//Vedi sol1.c
-void esprimi_pareri(sondaggio *s, int th) {
-  int i, voto;
-  pthread_mutex_lock(&s->m);
-  printf("\n\n COMPILAZIONE QUESTIONARIO per lo Spettatore %d:\n", th); 
-  for(i=0;i<K; i++) {
-	printf("voto del film  %s [0,.. 10]? ", s->film[i]);
-	scanf("%d", &voto);
-	s->voti[i]+=voto;
- }
-  s->pareri++;
-  printf("FINE QUESTIONARIO per lo spettatore %d\n RISULTATI PARZIALI SONDAGGIO:\n", th);
-  for(i=0;i<K;i++) {
-	printf("Valutazione media film %s: %f\n", s->film[i], (float)(s->voti[i])/s->pareri);
-  }
-  pthread_mutex_unlock (&s->m);
-}
-
-//La barriera si "attiva" quando il numero dei sondaggi completati coincide con il numero di utenti e 
-//restituisce il film vincitore, tenendo bloccati gli altri processi fino a che non viene stabilito
-void passBarriera(barriera_sincro *b, sondaggio *s) {
-    sem_wait(&b->mb);
-    b->completati++;
-    if (b->completati==NUM_THREADS) {
-        int t, i_max=0; float max=0;
-        float media;
-        for(t=0; t<K;t++) {
-                media=(float) s->voti[t]/NUM_THREADS;
-                printf("Valutazione media del film n.%d (%s): %f\n", t+1, s->film[t], media);
-                if (media>max) {
-                    max=media;
-                    i_max=t;
-                }
-        }
-        printf("\n\nIL FILM VINCITORE E': %s, con voto %f !\n",  s->film[i_max], max);
-        s->indiceVincitore=i_max; 
-        sem_post(&b->barriera);
+// Initialize the survey database
+void initialize_survey(survey *s) {
+    int i;
+    s->opinions_count = 0;
+    for (i = 0; i < K; i++) {
+        printf("What is the name of movie number %d? ", i + 1);
+        scanf("%s", s->movies[i]);
+        s->ratings[i] = 0;
     }
-    sem_post(&b->mb);
-    sem_wait(&b->barriera);
-    sem_post(&b->barriera);
-    return;
-}
-        
-void visione(int th, sondaggio *s) {
-    pthread_mutex_lock(&s->m);
-    printf("thread %d sta eseguendo download del film %s .. \n", th, s->film[s->indiceVincitore]);
-    pthread_mutex_unlock(&s->m);
+    pthread_mutex_init(&s->mutex, NULL);
 }
 
-//Quando viene creato il thread esegue questa funzione e "tira su" la barriera, che si riaprirà solo alla fine
-void *spettatore(void *t) {
-    long tid, result=0;
-    tid = (long)t;
-    esprimi_pareri(&S, tid);
-    passBarriera(&B, &S);
-    visione(tid, &S);
-    pthread_exit((void*) result);
+// Initialize the barrier as empty
+void initialize_barrier(barrier_sync *b) {
+    sem_init(&b->barrier_mutex, 0, 1);
+    sem_init(&b->barrier_gate, 0, 0);
+    b->completed = 0;
 }
 
+// Function for a viewer (thread) to submit their ratings
+void submit_opinions(survey *s, int thread_id) {
+    int i, rating;
+    pthread_mutex_lock(&s->mutex);
+    printf("\n\n FILLING OUT QUESTIONNAIRE for Viewer %d:\n", thread_id);
+    for (i = 0; i < K; i++) {
+        printf("Rating for movie %s [0 to 10]? ", s->movies[i]);
+        scanf("%d", &rating);
+        s->ratings[i] += rating;
+    }
+    s->opinions_count++;
+    printf("END OF QUESTIONNAIRE for Viewer %d\n PARTIAL SURVEY RESULTS:\n", thread_id);
+    for (i = 0; i < K; i++) {
+        printf("Average rating for movie %s: %f\n", s->movies[i], (float)(s->ratings[i]) / s->opinions_count);
+    }
+    pthread_mutex_unlock(&s->mutex);
+}
 
-int main (int argc, char *argv[]) {
-   pthread_t thread[NUM_THREADS];
-   int rc, i_max;
-   long t;
-   float media, max;
-   void *status;
-   init(&S);
-   initBarriera(&B);
-   //Rispetto a sol1.c non cambia niente in questa zona, il cambiamento è insito degno ogni thread
-   for(t=0; t<NUM_THREADS; t++) {
-      rc = pthread_create(&thread[t], NULL, spettatore, (void *)t); 
-      if (rc) {
-         printf("ERRORE: %d\n", rc);
-         exit(-1);   
-      }
-   }
-   for(t=0; t<NUM_THREADS; t++) {
-      rc = pthread_join(thread[t], &status);
-      if (rc) 
-          printf("ERRORE join thread %ld codice %d\n", t, rc);
-   }
-  return 0;
+// Barrier implementation
+void pass_barrier(barrier_sync *b, survey *s) {
+    sem_wait(&b->barrier_mutex);
+    b->completed++;
+
+    // If all threads have reached the barrier, determine the winning movie
+    if (b->completed == NUM_THREADS) {
+        int t, max_index = 0;
+        float max_rating = 0, avg_rating;
+
+        for (t = 0; t < K; t++) {
+            avg_rating = (float) s->ratings[t] / NUM_THREADS;
+            printf("Average rating for movie %d (%s): %f\n", t + 1, s->movies[t], avg_rating);
+            if (avg_rating > max_rating) {
+                max_rating = avg_rating;
+                max_index = t;
+            }
+        }
+        printf("\n\nTHE WINNING MOVIE IS: %s, with a rating of %f!\n", s->movies[max_index], max_rating);
+        s->winning_index = max_index;
+        sem_post(&b->barrier_gate);
+    }
+    sem_post(&b->barrier_mutex);
+
+    // Ensure all threads wait until the barrier is released
+    sem_wait(&b->barrier_gate);
+    sem_post(&b->barrier_gate);
+}
+
+// Simulate viewing the winning movie
+void watch_movie(int thread_id, survey *s) {
+    pthread_mutex_lock(&s->mutex);
+    printf("Thread %d is downloading the movie %s...\n", thread_id, s->movies[s->winning_index]);
+    pthread_mutex_unlock(&s->mutex);
+}
+
+// Function executed by each thread
+void *viewer(void *thread_data) {
+    long thread_id = (long)thread_data;
+    submit_opinions(&Survey, thread_id);
+    pass_barrier(&Barrier, &Survey);
+    watch_movie(thread_id, &Survey);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t threads[NUM_THREADS];
+    int rc;
+    long t;
+
+    // Initialize the survey and barrier
+    initialize_survey(&Survey);
+    initialize_barrier(&Barrier);
+
+    // Create threads
+    for (t = 0; t < NUM_THREADS; t++) {
+        rc = pthread_create(&threads[t], NULL, viewer, (void *)t);
+        if (rc) {
+            printf("ERROR: Unable to create thread %ld, error code %d\n", t, rc);
+            exit(-1);
+        }
+    }
+
+    // Wait for all threads to complete
+    for (t = 0; t < NUM_THREADS; t++) {
+        rc = pthread_join(threads[t], NULL);
+        if (rc) {
+            printf("ERROR joining thread %ld, error code %d\n", t, rc);
+        }
+    }
+
+    return 0;
 }
